6af5ce388fb904f7ce92be7f7507c027f25ec1e2
Alger <AlgerDu@outlook.com>
2017-03-04 23:05:29 +0800

    路径修改

--- Src/ClientApp/config/helpers.js
+++ Src/ClientApp/config/helpers.js
@@ -1,5 +1,5 @@
 var path = require('path');
-var _root = path.resolve(__dirname, '../../Server');
+var _root = path.resolve(__dirname, '..');
 function root(args) {
     args = Array.prototype.slice.call(arguments, 0);
     return path.join.apply(path, [_root].concat(args));
