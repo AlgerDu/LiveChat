3bf5786feaca3e23da0aba8eb04f43314addcec2
Alger <AlgerDu@outlook.com>
2017-03-05 23:51:34 +0800

    划分新的组件：导航

--- Src/ClientApp/src/app/app.module.ts
+++ Src/ClientApp/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './blocks/nav.component'
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
