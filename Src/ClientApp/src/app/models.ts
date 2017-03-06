bf3e924dfb345319ca37ef54aa56956bcd64a5fb
Alger <AlgerDu@outlook.com>
2017-03-06 21:40:55 +0800

    定义一些需要用到的实体模型

--- Src/ClientApp/src/app/models.ts
+++ Src/ClientApp/src/app/models.ts
@@ -4,4 +4,22 @@ export class ChatRecord {
     toUserId: string;
     sendTime: Date;
     text: string;
+    isRead: boolean;
+}
+
+export class Account {
+    id: number;
+    name: string;
+    chattingUsers: number[];
+    joinGroups: number[];
+}
+
+export class UserInfo {
+    id: number;
+    name: string;
+}
+
+export class Group {
+    id: number;
+    name: string;
 }
